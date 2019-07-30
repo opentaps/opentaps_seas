ad79f197aa20e0b544f2c6c0fac9c189053edc06
Konstantin Rybalko <konstantin.rybalko@gmail.com>
2019-07-30 16:52:39 +0300

    Restrict by ahu tag

--- opentaps_seas/core/views.py
+++ opentaps_seas/core/views.py
@@ -1735,6 +1735,12 @@ class EquipmentDetailView(LoginRequiredMixin, WithFilesAndNotesAndTagsMixin, Wit
             pass
 
         context['data_points'] = PointView.objects.filter(equipment_id=context['object'].object_id).count()
+        is_ahu = 0
+        if 'ahu' in context['object'].m_tags:
+            is_ahu = 1
+
+        context['is_ahu'] = is_ahu
+
         return context
 
 
