0cce7a75b6ca1fc4dacff659e176cbb07d3979e5
Konstantin Rybalko <konstantin.rybalko@gmail.com>
2019-12-03 16:18:30 +0200

    Fix import data script

--- scripts/import_unit_of_measure.py
+++ scripts/import_unit_of_measure.py
@@ -26,9 +26,10 @@ def clean():
     print('Deleting data ...')
     with connections['default'].cursor() as c:
         # delete all related tables first_row
+        c.execute("DELETE FROM core_meter_history;")
+        c.execute("DELETE FROM core_meter;")
         c.execute("DELETE FROM core_weather_history;")
         c.execute("DELETE FROM core_weather_station;")
-        c.execute("DELETE FROM core_meter_history;")
         c.execute("DELETE FROM core_unit_of_measure;")
         c.close()
 
