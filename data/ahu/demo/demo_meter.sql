24565fb1c0ae18387acddc0bb5caaef695f925d7
Konstantin Rybalko <konstantin.rybalko@gmail.com>
2019-12-11 13:25:18 +0200

    Add demo meter for second site

--- data/ahu/demo/demo_meter.sql
+++ data/ahu/demo/demo_meter.sql
@@ -1,2 +1,4 @@
 INSERT INTO public.core_meter(meter_id, description, from_datetime, thru_datetime, site_id, weather_station_id)
 VALUES ('demo-meter', 'demo meter', now(), null, 'demo-site-1', 'USAF_722874');
+INSERT INTO public.core_meter(meter_id, description, from_datetime, thru_datetime, site_id, weather_station_id)
+VALUES ('demo-meter-2', 'demo meter 2', now(), null, 'econ-demo-site-a', 'USAF_724010');
