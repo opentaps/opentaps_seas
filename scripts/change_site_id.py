0b2b68c455627874be897c4f8ab65aa68ba0ad9b
Jeremy Wickersheimer <jwickers@gmail.com>
2020-01-20 19:33:23 +0800

    Add script to change a site ID
    Note: can be adapated later to change other entities

---
+++ scripts/change_site_id.py
@@ -0,0 +1,75 @@
+# This file is part of opentaps Smart Energy Applications Suite (SEAS).
+
+# opentaps Smart Energy Applications Suite (SEAS) is free software:
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# opentaps Smart Energy Applications Suite (SEAS) is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with opentaps Smart Energy Applications Suite (SEAS).
+# If not, see <https://www.gnu.org/licenses/>.
+
+from django.db import connections
+from opentaps_seas.core.models import Entity
+
+
+def cleanup_crate(from_id):
+    # cleanup Crate DB since hte sync code does not expect the IDs to change
+    with connections['crate'].cursor() as c:
+        try:
+            c.execute("""DELETE FROM "volttron"."topic" WHERE kv_tags['id'] = %s ;""", [from_id])
+        except Exception as e:
+            print(e)
+
+        try:
+            c.execute("""DELETE FROM "volttron"."topic" WHERE kv_tags['siteRef'] = %s ;""", [from_id])
+        except Exception as e:
+            print(e)
+
+        c.close()
+
+
+def change_site_id(from_id, to_id):
+    site = None
+    try:
+        site = Entity.objects.get(kv_tags__id=from_id)
+    except Entity.DoesNotExist:
+        try:
+            site = Entity.objects.get(entity_id=from_id)
+            if site:
+                print('Site not found with ID "{}"" did you mean "{}" ?'.format(from_id, site.kv_tags.get('id')))
+                return
+        except Entity.DoesNotExist:
+            print('Site not found with ID "{}"'.format(from_id))
+            return
+
+    print("Found Site {}".format(site))
+
+    # change the site ID
+    site.kv_tags['id'] = to_id
+    site.save()
+
+    # change all related Entities:
+    for entity in Entity.objects.filter(kv_tags__siteRef=from_id):
+        print("Updating related Entity {}".format(entity))
+        entity.kv_tags['siteRef'] = to_id
+        entity.save()
+
+    cleanup_crate(from_id)
+
+
+def print_help():
+    print("Usage: python manage.py runscript chnage_site_id from_id to_id")
+
+
+def run(*args):
+    if len(args) == 2:
+        change_site_id(args[0], args[1])
+    else:
+        print_help()
